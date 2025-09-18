3d13625e1a08d9187f13bb29dc583b2dae834fca
Thành Trần <ratosan19@gmail.com>
2025-09-18 21:41:55 +0700

    feat: add .\QuyXanh\QuyXanh-main\src\components\UpToBeginToggle\index.jsx

---
+++ QuyXanh/QuyXanh/QuyXanh-main/src/components/UpToBeginToggle/index.jsx
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from "react";
+import classNames from "classnames/bind";
+import {useTranslation} from "react-i18next";
+import style from "./UpToBeginToggle.module.scss";
+import {FiArrowUp} from "react-icons/fi";
+
+const cx = classNames.bind(style);
+
+function UpToBeginToggle() {
+	const [isVisible, setIsVisible] = useState(false);
+	const {t} = useTranslation();
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollTop = window.pageYOffset;
+			const documentHeight = document.documentElement.scrollHeight;
+			const windowHeight = window.innerHeight;
+
+			const scrollPercentage = (scrollTop + windowHeight) / documentHeight;
+			setIsVisible(scrollPercentage > 0.8);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	const scrollToTop = () => {
+		window.dispatchEvent(new CustomEvent("scrollToTop"));
+
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
+
+	return (
+		<button
+			className={cx("up-to-begin-toggle", {visible: isVisible})}
+			onClick={scrollToTop}
+			title={t("tooltip.backToTop")}
+		>
+			<FiArrowUp className={cx("arrow-icon")} />
+		</button>
+	);
+}
+
+export default UpToBeginToggle;
